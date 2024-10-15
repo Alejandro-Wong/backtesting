0876b0c96e895ec10a2eba0cfdd58a2c58f85b80
Alejandro_Wong <121511772+Alejandro-Wong@users.noreply.github.com>
2024-10-14 20:55:06 -0400

    adjusted comment positioning

--- ohlc.py
+++ ohlc.py
@@ -82,29 +82,29 @@ class GetOHLC:
         # Daily OHLCV requested
         if self.interval == '1d':
 
-        # Start and end dates specified
+            # Start and end dates specified
             if self.start != None and self.end != None:
                 start_date = self.start
                 end_date = self.end
 
-        # Start and end date not specified, period is in year ('y')
+            # Start and end date not specified, period is in year ('y')
             elif self.start == None and self.end == None and 'y' in self.period:
                 num_years = re.split('(\\d+)', self.period)
                 start_date = date.today() - timedelta(days=365*int(num_years[1]))
                 end_date = date.today() - timedelta(days=1)
                 
-        # Start and end date not specified, period is in days ('d')
+            # Start and end date not specified, period is in days ('d')
             elif self.start == None and self.end == None and 'd' in self.period:
                 num_days = re.split('(\\d+)', self.period)
                 start_date = date.today() - timedelta(days=int(num_days[1]))
                 end_date = date.today() - timedelta(days=1)
             
-        # Start and end date not specified, no period specified, default period is 5 years
+            # Start and end date not specified, no period specified, default period is 5 years
             else: 
                 start_date = date.today() - timedelta(days=1825)
                 end_date = date.today() - timedelta(days=1)
 
-        # Request daily bars
+            # Request daily bars
             request_params = StockBarsRequest(
             symbol_or_symbols = self.symbol,
             timeframe=TimeFrame.Day,
@@ -113,11 +113,11 @@ class GetOHLC:
             end=end_date
             )
 
-        # Raw DataFrame
+            # Raw DataFrame
             stock = client.get_stock_bars(request_params)
             raw_data = stock.df
 
-        # OHLC data rounded two decimal places, index renamed and set to pd.datetime format, unneeded columns dropped
+            # OHLC data rounded two decimal places, index renamed and set to pd.datetime format, unneeded columns dropped
             df = raw_data.reset_index(level=0, drop=True)
             df = df.round(self.two_decimals)
             df.rename(columns={'open':'Open','high':'High',
@@ -134,35 +134,35 @@ class GetOHLC:
         # Intraday OHLCV requested
         else:
             
-        # Start and end datetimes specified
+            # Start and end datetimes specified
             if self.start != None and self.end != None:
 
                 start_datetime = pd.to_datetime(self.start, utc=True)
                 end_datetime = pd.to_datetime(self.end, utc=True)
 
-        # Default 'max' to 5 years worth of data
+            # Default 'max' to 5 years worth of data
             elif self.start == None and self.end == None and self.period == 'max':
                 start_datetime = date.today() - timedelta(days=1825)
                 end_datetime = date.today() - timedelta(days=1)
             
-        # Request year-to-date's worth of data
+            # Request year-to-date's worth of data
             elif self.start == None and self.end == None and self.period == 'ytd':
                 start_datetime = date.today() - timedelta(days=365)
                 end_datetime = date.today() - timedelta(days=1)
 
-        # Request user defined number of years worth of data
+            # Request user defined number of years worth of data
             elif self.start == None and self.end == None and 'y' in self.period:
                 num_years = re.split('(\\d+)', self.period)
                 start_datetime=date.today() - timedelta(days=365*int(num_years[1]))
                 end_datetime = date.today() - timedelta(days=1)
             
-        # Request user defined number of days worth of data
+            # Request user defined number of days worth of data
             elif self.start == None and self.end == None and self.period != None:
                 num_days = re.split('(\\d+)', self.period)
                 start_datetime = date.today() - timedelta(days=int(num_days[1]))
                 end_datetime = date.today() - timedelta(days=1)
 
-        # Request user defined number of days from specified end date          
+            # Request user defined number of days from specified end date          
             elif self.start == None and self.end != None and self.period != None:
                 if 'y' in self.period:
                     num_years = re.split('(\\d+)', self.period)
@@ -173,7 +173,7 @@ class GetOHLC:
                     end_datetime = pd.to_datetime(self.end)
                     start_datetime = end_datetime - timedelta(days=int(num_days[1]))
 
-        # If no start or end datetime given, default to 100 days worth of data
+            # If no start or end datetime given, default to 100 days worth of data
             else:
                 start_datetime = date.today() - timedelta(days=100)
                 end_datetime = date.today() - timedelta(days=1)
@@ -185,14 +185,14 @@ class GetOHLC:
                 amount = 30
                 end = '15:30'
 
-        # Intraday interval determines last printed time value i.e '5m' data ends at 15:55
+            # Intraday interval determines last printed time value i.e '5m' data ends at 15:55
             else: 
                 res = re.split('(\\d+)', self.interval)
                 amount = int(res[1])
                 endtime = timedelta(hours=16, minutes=00)
                 end = str(endtime - timedelta(minutes=amount))
    
-        # Request intraday bars
+            # Request intraday bars
             request_params = StockBarsRequest(
                 symbol_or_symbols = self.symbol,
                 timeframe=TimeFrame(amount=amount, unit=TimeFrameUnit.Minute),
@@ -200,11 +200,11 @@ class GetOHLC:
                 start=start_datetime,
                 end=end_datetime
             )
-        # Raw DataFrame
+            # Raw DataFrame
             stock = client.get_stock_bars(request_params)
             raw_data = stock.df
 
-        # Reset index, fix timezone, set index to pd.datetime format, rename index
+            # Reset index, fix timezone, set index to pd.datetime format, rename index
             df = raw_data.reset_index(level=0, drop=True)
             df = df[~df.index.duplicated(keep='first')]         # in case of duplicate index
             df.index = pd.to_datetime(df.index).strftime('%Y-%m-%d %H:%M:%S')
@@ -214,24 +214,24 @@ class GetOHLC:
             df.index.names = ['Datetime']
 
 
-        # Fix column names, drop unneeded columns
+            # Fix column names, drop unneeded columns
             df = df.drop(columns=['trade_count','vwap'])
             df = df.rename(columns={'open':'Open','high':'High',
                             'low':'Low','close':'Close',
                             'volume':'Volume'})
 
-        # Convert Volume column to integer format
+            # Convert Volume column to integer format
             df['Volume'] = df['Volume'].astype(int)
 
-        # Round OHLC data to two decimals
+            # Round OHLC data to two decimals
             df = df.round(self.two_decimals)
 
-        # Filter data to only show ohlcv during open market hours (9:30 - 4:00)
+            # Filter data to only show ohlcv during open market hours (9:30 - 4:00)
             df = df.between_time('09:30', end)
             # print(df.head())
 
 
-        # Resample 30m to 1h
+            # Resample 30m to 1h
             if self.interval == '1h':
                 df = df.resample('1h', offset='30Min').apply(ohlc_agg).dropna().between_time('09:30', '15:30')
                 return df     
